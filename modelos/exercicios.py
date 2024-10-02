0391a7961009cabb2e19b574bfc27867aba8e7c7
Bowie <thiiago.limaa@gmail.com>
2024-10-02 07:14:16 -0300

    iniciando projeto

---
+++ modelos/exercicios.py
@@ -0,0 +1,11 @@
+"""
+Exercícios
+Atribua o valor 'Italiana' ao atributo categoria da instância restaurante_praca da classe Restaurante.
+Acesse o valor do atributo nome da instância restaurante_praca da classe Restaurante.
+Verifique o valor inicial do atributo ativo para a instância restaurante_praca e exiba uma mensagem informando se o restaurante está ativo ou inativo.
+Acesse o valor do atributo de classe categoria diretamente da classe Restaurante e armazene em uma variável chamada categoria.
+Altere o valor do atributo nome para 'Bistrô'.
+Crie uma nova instância da classe Restaurante chamada restaurante_pizza com o nome 'Pizza Place' e categoria 'Fast Food'.
+Verifique se a categoria da instância restaurante_pizza é 'Fast Food'.
+Mude o estado da instância restaurante_pizza para ativo.
+Imprima no console o nome e a categoria da instância restaurante_praca."""
